48d7cacadfc79a2d8d59728b3ab11d98c842bbf5
Maxime Mouchet <max@maxmouchet.com>
2022-07-10 09:50:47 +0200

    faster iter_lines

--- httpx/_decoders.py
+++ httpx/_decoders.py
@@ -259,61 +259,25 @@ class LineDecoder:
     Handles incrementally reading lines from text.
 
     Uses universal line decoding, supporting any of `\n`, `\r`, or `\r\n`
-    as line endings, normalizing to `\n`.
+    as line endings.
+    https://docs.python.org/3/library/stdtypes.html#str.splitlines
     """
 
     def __init__(self) -> None:
         self.buffer = ""
 
     def decode(self, text: str) -> typing.List[str]:
-        lines = []
-
-        if text and self.buffer and self.buffer[-1] == "\r":
-            if text.startswith("\n"):
-                # Handle the case where we have an "\r\n" split across
-                # our previous input, and our new chunk.
-                lines.append(self.buffer[:-1] + "\n")
-                self.buffer = ""
-                text = text[1:]
-            else:
-                # Handle the case where we have "\r" at the end of our
-                # previous input.
-                lines.append(self.buffer[:-1] + "\n")
-                self.buffer = ""
-
-        while text:
-            num_chars = len(text)
-            for idx in range(num_chars):
-                char = text[idx]
-                next_char = None if idx + 1 == num_chars else text[idx + 1]
-                if char == "\n":
-                    lines.append(self.buffer + text[: idx + 1])
-                    self.buffer = ""
-                    text = text[idx + 1 :]
-                    break
-                elif char == "\r" and next_char == "\n":
-                    lines.append(self.buffer + text[:idx] + "\n")
-                    self.buffer = ""
-                    text = text[idx + 2 :]
-                    break
-                elif char == "\r" and next_char is not None:
-                    lines.append(self.buffer + text[:idx] + "\n")
-                    self.buffer = ""
-                    text = text[idx + 1 :]
-                    break
-                elif next_char is None:
-                    self.buffer += text
-                    text = ""
-                    break
-
+        if self.buffer:
+            text = self.buffer + text
+        lines = text.splitlines()
+        if lines and lines[-1] and text and lines[-1][-1] == text[-1]:
+            self.buffer = lines.pop()
+        else:
+            self.buffer = ""
         return lines
 
     def flush(self) -> typing.List[str]:
-        if self.buffer.endswith("\r"):
-            # Handle the case where we had a trailing '\r', which could have
-            # been a '\r\n' pair.
-            lines = [self.buffer[:-1] + "\n"]
-        elif self.buffer:
+        if self.buffer:
             lines = [self.buffer]
         else:
             lines = []
